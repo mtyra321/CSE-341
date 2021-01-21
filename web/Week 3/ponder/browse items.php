e3fb9e60aacc4493acf41db1e81dd9e3cd0adebf
mtyra321 <mtyra321@gmail.com>
2021-01-21 14:43:29 -0700

    added pictures and formatting

--- web/Week 3/ponder/browse items.php
+++ web/Week 3/ponder/browse items.php
@@ -9,6 +9,29 @@
 </head>
 <body>
     <h1>Store</h1>
+    <form action="checkout.php" method="POST">
+    <div class="grid-container">
 
+    <div class="grid-item name">
+            <p id="name">Matt Tyra </p>
+        </div>
+        <div class="grid-item class">
+
+            <p id="class">Web Backend 2 </p>
+        </div>
+        <div class="grid-item column-1">
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card"><br>
+            <input type="checkbox" id="N gift 10" name="items" value="N gift 10">
+            <label for="Africa">Africa </label><br>
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card">
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card">
+        </div>
+        <div class="grid-item column-2">
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card">
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card">
+            <img src="N gift 10.jpg" alt="nintendo $10 gift card">
+        </div>
+    </div>
+    </form>
 </body>
 </html>
