f0a0e49e5d7351dbb1261aef22b1c8d517e81956
mtyra321 <mtyra321@gmail.com>
2021-02-09 12:39:28 -0700

    a

--- web/Week 6/team act.php
+++ web/Week 6/team act.php
@@ -54,43 +54,43 @@ $db = get_db();
 // a list of topics. But to keep things as clear as possible we can
 // also query and loop through the results, right here.
 
-try
-{
-	// Notice that we do not use "SELECT *" here. It is best practice
-	// to only bring back the fields that you need.
+// try
+// {
+// 	// Notice that we do not use "SELECT *" here. It is best practice
+// 	// to only bring back the fields that you need.
 
-	// prepare the statement
-	$statement = $db->prepare('SELECT id, name FROM scripture');
-	$statement->execute();
+// 	// prepare the statement
+// 	$statement = $db->prepare('SELECT id, name FROM scripture');
+// 	$statement->execute();
 
-	// Go through each result
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-		$id = $row['id'];
-		$name = $row['name'];
+// 	// Go through each result
+// 	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+// 	{
+// 		$id = $row['id'];
+// 		$name = $row['name'];
 
-		// Notice that we want the value of the checkbox to be the id of the label
-		echo "<input type='checkbox' name='chkTopics[]' id='chkTopics$id' value='$id'>";
+// 		// Notice that we want the value of the checkbox to be the id of the label
+// 		echo "<input type='checkbox' name='chkTopics[]' id='chkTopics$id' value='$id'>";
 
-		// Also, so they can click on the label, and have it select the checkbox,
-		// we need to use a label tag, and have it point to the id of the input element.
-		// The trick here is that we need a unique id for each one. In this case,
-		// we use "chkTopics" followed by the id, so that it becomes something like
-		// "chkTopics1" and "chkTopics2", etc.
-		echo "<label for='chkTopics$id'>$name</label><br />";
+// 		// Also, so they can click on the label, and have it select the checkbox,
+// 		// we need to use a label tag, and have it point to the id of the input element.
+// 		// The trick here is that we need a unique id for each one. In this case,
+// 		// we use "chkTopics" followed by the id, so that it becomes something like
+// 		// "chkTopics1" and "chkTopics2", etc.
+// 		echo "<label for='chkTopics$id'>$name</label><br />";
 
-		// put a newline out there just to make our "view source" experience better
-		echo "\n";
-	}
+// 		// put a newline out there just to make our "view source" experience better
+// 		echo "\n";
+// 	}
 
-}
-catch (PDOException $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
+// }
+// catch (PDOException $ex)
+// {
+// 	// Please be aware that you don't want to output the Exception message in
+// 	// a production environment
+// 	echo "Error connecting to DB. Details: $ex";
+// 	die();
+// }
 
 ?>
 
