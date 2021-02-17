467b5f468bc294701d8271bf90443b7ff53dc3a3
mtyra321 <mtyra321@gmail.com>
2021-02-17 13:32:30 -0700

    awd

--- web/Week 6/individual_bathroom.php
+++ web/Week 6/individual_bathroom.php
@@ -19,7 +19,7 @@ $db = get_db();
 	$id = $_POST["id"];
     echo 'id is ', $id;
     // prepare the statement
-	$statement = $db->prepare('SELECT BathroomID, RoomNumber, Gender, description, building, overallrating FROM bathroom');
+	$statement = $db->prepare('SELECT BathroomID, RoomNumber, Gender, description, building, overallrating FROM bathroom Where BathroomID = 4');
 	$statement->execute();
 	$bathrooms_list = [];
 
