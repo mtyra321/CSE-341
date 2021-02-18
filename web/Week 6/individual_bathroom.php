ffa37ef93a52b1126d504aa36495dc247dd03244
mtyra321 <mtyra321@gmail.com>
2021-02-18 14:49:06 -0700

    b

--- web/Week 6/individual_bathroom.php
+++ web/Week 6/individual_bathroom.php
@@ -38,13 +38,14 @@ $db = get_db();
 
 
 ?>
-<button><a href="./show_bathrooms.php">Back to List</a></button>
+<button onclick = "./show_bathrooms.php">Back to List</button>
 <button onclick="delete_bathroom()">Delete Bathroom</button>
 <script type="text/javascript">
 function delete_bathroom(){
 console.log("deleting bathroom");
 <?php $statement = $db->prepare('DELETE FROM bathroom Where BathroomID = '.$id);
 	$statement->execute();?>
+	window.locatio.href = "./show_bathrooms.php"
 }
 </script>
 </div>
